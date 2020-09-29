a610ee135aa2158868a12fc57c9308640da6d3d2
Steven Tran <steven.tran15@hotmail.com>
2020-09-28 22:59:42 -0400

    Fixed redirect to home screen after sign up

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, Button, Dimensions, StyleSheet, Keyboard } from 'react-native';
 
 import * as Animatable from 'react-native-animatable';
@@ -9,7 +9,7 @@ import Background from '../components/background';
 var screenHeight = Dimensions.get("window").height;
 var screenWidth = Dimensions.get("window").width;
 
-const SignUpScreen = ({navigation}) => {
+const SignUpScreen = ({ navigation }) => {
     const [fullName, setFullName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -35,7 +35,7 @@ const SignUpScreen = ({navigation}) => {
                     .doc(uid)
                     .set(data)
                     .then(() => {
-                        navigation.navigate('Home')
+                        navigation.navigate('HomeScreen')
                     })
                     .catch((error) => {
                         alert(error)
@@ -43,31 +43,31 @@ const SignUpScreen = ({navigation}) => {
             })
             .catch((error) => {
                 alert(error)
-        });
+            });
     }
 
     return (
         <View style={styles.container}>
-            <Background/>
-            <Animatable.View style={styles.drawer} animation="fadeInUpBig"> 
+            <Background />
+            <Animatable.View style={styles.drawer} animation="fadeInUpBig">
                 <Image style={styles.headerImage} source={require('../assets/android/drawable-mdpi/login-logo.png')} />
                 <Text style={styles.headerFont}> Sign-Up </Text>
-                <View style={styles.whitePadding}/>
+                <View style={styles.whitePadding} />
                 <TextInput style={styles.textInput} placeholder="Full Name" autoCapitalize="none" onChangeText={(text) => setFullName(text)}
-                    value={fullName} returnKeyType='done' onSubmitEditing={Keyboard.dismiss}/>
+                    value={fullName} returnKeyType='done' onSubmitEditing={Keyboard.dismiss} />
                 <TextInput style={styles.textInput} placeholder="Email Address" autoCapitalize="none" onChangeText={(text) => setEmail(text)}
-                    value={email} returnKeyType='done' onSubmitEditing={Keyboard.dismiss}/> 
-                <TextInput style={styles.textInput} secureTextEntry placeholder="Password" autoCapitalize="none"  onChangeText={(text) => setPassword(text)}
-                    value={password} returnKeyType='done' onSubmitEditing={Keyboard.dismiss}/>
+                    value={email} returnKeyType='done' onSubmitEditing={Keyboard.dismiss} />
+                <TextInput style={styles.textInput} secureTextEntry placeholder="Password" autoCapitalize="none" onChangeText={(text) => setPassword(text)}
+                    value={password} returnKeyType='done' onSubmitEditing={Keyboard.dismiss} />
                 <TextInput style={styles.textInput} secureTextEntry placeholder="Confirm Password" autoCapitalize="none" placeholder='Confirm Password'
-                    onChangeText={(text) => setConfirmPassword(text)} returnKeyType='done' onSubmitEditing={Keyboard.dismiss}/>
+                    onChangeText={(text) => setConfirmPassword(text)} returnKeyType='done' onSubmitEditing={Keyboard.dismiss} />
 
-                <TouchableOpacity style={styles.button} onPress={()=>onRegisterPress()}>
+                <TouchableOpacity style={styles.button} onPress={() => onRegisterPress()}>
                     <Image source={require('../assets/android/drawable-mdpi/g-login-arrow.png')} />
                 </TouchableOpacity>
-                <View style={styles.whitePadding}/>
+                <View style={styles.whitePadding} />
                 <Text style={styles.descriptionFont}> Already have an account? </Text>
-                <TouchableOpacity onPress={()=>navigation.push('SignInScreen')}> 
+                <TouchableOpacity onPress={() => navigation.push('SignInScreen')}>
                     <Text style={styles.clickableFont}> SIGN IN </Text>
                 </TouchableOpacity>
             </Animatable.View>
@@ -79,56 +79,56 @@ const SignUpScreen = ({navigation}) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center', 
-    }, 
+        justifyContent: 'center',
+    },
     heading: {
-        flex: 1, 
-        justifyContent: 'flex-end', 
-        paddingHorizontal: 20, 
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
         paddingBottom: 5
     },
     headerFont: {
         fontFamily: 'roboto-regular',
         fontSize: 32,
-        fontWeight: "100", 
-        left: screenWidth/3.5, 
+        fontWeight: "100",
+        left: screenWidth / 3.5,
         top: screenHeight * 0.07,
         paddingBottom: 30
     },
     headerImage: {
-        position: 'absolute', 
-        left: screenWidth/20, 
+        position: 'absolute',
+        left: screenWidth / 20,
         top: screenHeight * 0.07
     },
     whitePadding: {
-        height: screenHeight/8
+        height: screenHeight / 8
     },
     textInput: {
-        borderBottomColor: 'rgba(112, 112, 112, 0.7)', 
+        borderBottomColor: 'rgba(112, 112, 112, 0.7)',
         borderBottomWidth: 1.5,
-        fontSize: 16, 
+        fontSize: 16,
         paddingTop: 8
     },
     descriptionFont: {
-        fontFamily: 'roboto-regular', 
-        fontSize: 12, 
+        fontFamily: 'roboto-regular',
+        fontSize: 12,
         color: 'rgba(0, 0, 0, 0.38)'
     },
     clickableFont: {
         fontFamily: 'roboto-medium',
-        fontSize: 14, 
+        fontSize: 14,
     },
-    button: { 
+    button: {
         paddingRight: 30,
         marginTop: 30
-    }, 
+    },
     drawer: {
         flex: 4,
-        backgroundColor: '#fff', 
-        borderTopLeftRadius: 30, 
-        borderTopRightRadius: 30, 
-        paddingVertical: 50, 
-        paddingHorizontal: 30, 
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingVertical: 50,
+        paddingHorizontal: 30,
         position: 'absolute',
         width: screenWidth,
         height: screenHeight * 0.85,
