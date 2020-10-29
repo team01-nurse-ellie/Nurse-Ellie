2689e6255ed1477ecf967d6e46f2ef3a0b3f5fc7
Steven Tran <steven.tran15@hotmail.com>
2020-10-29 13:30:36 -0400

    Able to send push notifications to a device

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, KeyboardAvoidingView, TouchableOpacity, Button, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, KeyboardAvoidingView, TouchableOpacity, Button, Dimensions, StyleSheet, Platform, } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import Constants from 'expo-constants';
 
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,8 +13,21 @@ import Background from '../components/background';
 import MenuIcon from '../assets/images/menu-icon.svg';
 
 const HomeScreen = ({ navigation }) => {
+
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: false,
+          shouldSetBadge: false,
+        }),
+      });
+      
     const [greeting, setGreeting] = useState('');
+    const [pushToken, setToken] = useState('');
+
     useEffect(()=> {
+        registerForPushNotificationsAsync();
+        sendPushNotif();
         var hours = new Date().getHours();
         if (hours < 12) {
             setGreeting('Good morning');
@@ -21,6 +37,69 @@ const HomeScreen = ({ navigation }) => {
             setGreeting('Good evening');
         }
     }, []);
+
+    const sendPushNotif = async () => {
+        console.log("sendPushNotif();");
+
+        const pushNotif = [{
+            to: "ExponentPushToken[bWx33iCW7hAkBeWX3u6SkR]",
+            title: "MyTitle",
+            body: "This is the body",
+        }, {
+            to: "ExponentPushToken[bWx33iCW7hAkBeWX3u6SkR]",
+            title: "MyTitle2",
+            body: "This is the body2",
+        }];
+
+        await fetch("https://exp.host/--/api/v2/push/send", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(pushNotif)
+        }).then(res => res.json()).then(data=> {
+            console.log(data);
+        })
+
+        
+    };
+
+    const registerForPushNotificationsAsync = async () => {
+        if (Constants.isDevice) {
+            const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                console.log("ASK")
+                const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            const token = await Notifications.getExpoPushTokenAsync();
+            console.log(token);
+            // this.setState({ expoPushToken: token });
+            setToken(token);
+        } else {
+            alert('Must use physical device for Push Notifications');
+        }
+
+        if (Platform.OS === 'android') {
+            console.log("setting channel")
+            Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        Notifications.getNotificationChannelsAsync().then(arr => {
+            // console.log(arr); 
+        })
+    };
+
 
     return (
         <KeyboardAvoidingView style={styles.background} behaviour="padding" enabled>
