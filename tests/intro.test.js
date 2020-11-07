02295e85ad5f083cb3892665badd384c44220757
vchiuu <vivian.chiu@live.com>
2020-11-07 13:19:48 -0500

    added snapshots to intro

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -4,6 +4,8 @@ import renderer from 'react-test-renderer';
 import Background from '../components/background';
 import BackgroundHP from '../components/backgroundHP';
 import SplashScreen from '../screens/SplashScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 test('Patient background component renders correctly', ()=>{
     const wrapper = renderer.create(<Background/>).toJSON();
@@ -17,4 +19,15 @@ test('Health Professional background component renders correctly', ()=>{
 
 test('Splash Screen renders correctly', ()=>{
     const wrapper = renderer.create(<SplashScreen/>);
+    expect(wrapper).toMatchSnapshot();
 });
+
+test('Sign In Screen renders correctly', ()=>{
+    const wrapper = renderer.create(<SignInScreen/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Sign Up Screen renders correctly', ()=>{
+    const wrapper = renderer.create(<SignUpScreen/>);
+    expect(wrapper).toMatchSnapshot();
+})
